d5b55664720a1d5205dcec4ebe477a99d8c8b01a
dinyar-qa <dinyar.qa@gmail.com>
2024-09-16 00:50:38 -0400

    use standard viewport sizes

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ const dotenvPlugin = require('cypress-dotenv');
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://m4n4gemy.app/",
+    viewportHeight: 1080,
+    viewportWidth: 1920,
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
