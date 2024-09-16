e2b3090cfeaa948700cae70bedb86277a6cf7f81
dinyar-qa <dinyar.qa@gmail.com>
2024-09-16 00:45:40 -0400

    add a simple login test

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const dotenvPlugin = require('cypress-dotenv');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://m4n4gemy.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('m4n4ge-my-app', () => {
+  it('logs in successfully', () => {
+    cy.visit('/')
+
+    //click on the sign in button
+    cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
+
+    //fill the email and password fields and click on the sign in button
+    cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
+    cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
+    cy.contains('button', 'Sign In').click()
+
+    //check if the user is redirected to the dashboard
+    cy.location('pathname').should('equal', '/dashboard')
+
+    //check if the demo user banner is displayed
+    cy.get('.css-14mo1hq').should('be.visible')
   })
 })
