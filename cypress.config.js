9341cd67282d4fd7e24181c266c71cb4b5471a08
dinyar-qa <dinyar.qa@gmail.com>
2024-09-16 00:44:39 -0400

    install and use cypress-dotenv

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const dotenvPlugin = require('cypress-dotenv');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      // Load environment variables from .env file
+      config = dotenvPlugin(config);
+      return config;
     },
   },
 });
