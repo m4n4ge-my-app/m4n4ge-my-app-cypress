a7315de726a112fa4a2dee8d3dd041c67626d216
dinyar-qa <dinyar.qa@gmail.com>
2024-09-17 02:46:43 -0400

    format and typo fix

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -14,28 +14,28 @@ describe('m4n4ge-my-app: dashboard tests', () => {
   })
 
   it('logs in successfully', () => {
-    //check if the user is redirected to the dashboard
+    // Check if the user is redirected to the dashboard
     cy.location('pathname').should('equal', '/dashboard')
 
-    //check if the demo user banner is displayed
+    // Check if the demo user banner is displayed
     cy.get('.css-14mo1hq').should('be.visible')
   })
 
   it('should prevent demo user from adding a new application', () => {
-    //close/acknowledge the demo user banner
+    // Close/acknowledge the demo user banner
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
-    //click on the add new application button
+    // Click on the add new application button
     cy.get('.MuiFab-root').click().wait(1000)
 
-    //check if the user is redirected to the new application page
+    // Check if the user is redirected to the new application page
     cy.location('pathname').should('equal', '/add')
     cy.contains('h6', 'Add Application Record').should('be.visible')
 
-    //check if the required fields are working by directly submitting the form without filling the fields
+    // Check if the required fields are working by directly submitting the form without filling the fields
     cy.contains('button', 'Add Application').click()
 
-    //check if certains fields are required and "... is required" error message is displayed
+    // Check if certain fields are required and "... is required" error message is displayed
     cy.get('label').filter('.Mui-error').its('length').should('be.gt', 1)
     cy.get('body *').filter(':contains("is required")').each($el => {
       cy.wrap($el).should('be.visible')
@@ -43,95 +43,95 @@ describe('m4n4ge-my-app: dashboard tests', () => {
       expect($visibleElements.length).to.be.gt(1)
     })
 
-    //fill just the required fields and submit the form
+    // Fill just the required fields and submit the form
     cy.get('input[name="employerName"]').type('Test Employer')
     cy.get('input[name="positionName"]').type('Test Application')
     cy.get('div.MuiSelect-outlined').click().wait(500)
     cy.contains('li', 'Indeed').click()
     cy.contains('button', 'Add Application').click()
 
-    //check if the user actions gets blocked and the error message is displayed
+    // Check if the user actions get blocked and the error message is displayed
     cy.contains('div', 'Access Denied: Demonstration accounts do not have the privileges to add an application. Please create a personal account for full access.').should('be.visible')
     cy.location('pathname').should('equal', '/add')
   })
 
-  it('should prevent demo user from deleting a new application while on dashboad page', () => {
-    //switch to expert user account as new user have no applications to delete
+  it('should prevent demo user from deleting a new application while on dashboard page', () => {
+    // Switch to expert user account as new user has no applications to delete
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
-    //close/acknowledge the demo user banner
+    // Close/acknowledge the demo user banner
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
-    //check if expert user data is loaded
+    // Check if expert user data is loaded
     cy.contains('h6', 'John').should('be.visible')
     cy.get('div.MuiAvatar-root').should('contain', 'J')
 
-    //find the fist application and click on it to get the delete button  and click it
+    // Find the first application and click on it to get the delete button and click it
     cy.get('tbody>tr').first().click().contains('button', 'Delete').click()
 
-    //cofirm that delete confirmation modal appears
+    // Confirm that delete confirmation modal appears
     cy.contains('h2#modal-modal-title', 'Delete Application').should('be.visible')
     cy.contains('p#modal-modal-description', 'Are you sure you want to delete').should('be.visible')
 
-    //click on the proceed button in the
+    // Click on the proceed button in the modal
     cy.contains('button', 'Proceed').click()
 
-    //check if the user actions gets blocked and the error message is displayed
+    // Check if the user actions get blocked and the error message is displayed
     cy.contains('div', 'Access Denied: Demonstration accounts do not have the privileges to delete an application. Please create a personal account for full access.').should('be.visible')
   })
 
   it('should prevent demo user from deleting a new application while on edit application page', () => {
-    //switch to expert user account as new user have no applications to delete
+    // Switch to expert user account as new user has no applications to delete
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
-    //close/acknowledge the demo user banner
+    // Close/acknowledge the demo user banner
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
-    //check if expert user data is loaded
+    // Check if expert user data is loaded
     cy.contains('h6', 'John').should('be.visible')
     cy.get('div.MuiAvatar-root').should('contain', 'J')
 
-    //find the fist application and click on it to get the edit button  and click it
+    // Find the first application and click on it to get the edit button and click it
     cy.get('tbody>tr').first().click().contains('button', 'Edit').click()
 
-    //check if the user is redirected to the edit application page
+    // Check if the user is redirected to the edit application page
     cy.location('pathname').should('include', '/edit')
 
-    //click on delete button
+    // Click on delete button
     cy.contains('button', 'Delete').click()
 
-    //cofirm that delete confirmation modal appears
+    // Confirm that delete confirmation modal appears
     cy.contains('h2#modal-modal-title', 'Delete Application').should('be.visible')
     cy.contains('p#modal-modal-description', 'Are you sure you want to delete').should('be.visible')
 
-    //click on the proceed button in the
+    // Click on the proceed button in the modal
     cy.contains('button', 'Proceed').click()
 
-    //check if the user actions gets blocked and the error message is displayed
+    // Check if the user actions get blocked and the error message is displayed
     cy.contains('div', 'Access Denied: Demonstration accounts do not have the privileges to delete an application. Please create a personal account for full access.').should('be.visible')
   })
 
   it('should prevent demo user from editing an existing application', () => {
-    //switch to expert user account as new user have no applications to edit
+    // Switch to expert user account as new user has no applications to edit
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
-    //close/acknowledge the demo user banner
+    // Close/acknowledge the demo user banner
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
-    //find the fist application and click on it to get the edit button  and click it
+    // Find the first application and click on it to get the edit button and click it
     cy.get('tbody>tr').first().click().contains('button', 'Edit').click()
 
-    //check if the user is redirected to the edit application page
+    // Check if the user is redirected to the edit application page
     cy.location('pathname').should('include', '/edit')
 
-    //check if the employer name field has a value
+    // Check if the employer name field has a value
     cy.get('input[name="employerName"]').should('have.value', 'Zoom')
 
-    //change the employer name and submit the form
+    // Change the employer name and submit the form
     cy.get('input[name="employerName"]').clear().type('Test Employer')
     cy.contains('button', 'Save Changes').click()
 
-    //check if the user actions gets blocked and the error message is displayed
+    // Check if the user actions get blocked and the error message is displayed
     cy.contains('div', 'Access Denied: Demonstration accounts do not have the privileges to update an application. Please create a personal account for full access.').should('be.visible')
     cy.location('pathname').should('include', '/edit')
   })
