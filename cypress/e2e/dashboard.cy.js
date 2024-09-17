82e4dfff37348efc429dc8a9c63f7115692a3d49
dinyar-qa <dinyar.qa@gmail.com>
2024-09-17 02:44:16 -0400

    move repetitive login logic into hooks

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,15 +1,19 @@
 describe('m4n4ge-my-app: dashboard tests', () => {
-  it('logs in successfully', () => {
+  before(() => {
+    // Runs once before all tests in the block
     cy.visit('/')
+  })
 
-    //click on the sign in button
+  beforeEach(() => {
+    // Runs before each test in the block
+    cy.visit('/')
     cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
-
-    //fill the email and password fields and click on the sign in button
     cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
     cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
     cy.contains('button', 'Sign In').click()
+  })
 
+  it('logs in successfully', () => {
     //check if the user is redirected to the dashboard
     cy.location('pathname').should('equal', '/dashboard')
 
@@ -18,16 +22,6 @@ describe('m4n4ge-my-app: dashboard tests', () => {
   })
 
   it('should prevent demo user from adding a new application', () => {
-    cy.visit('/')
-
-    //click on the sign in button
-    cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
-
-    //fill the email and password fields and click on the sign in button
-    cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
-    cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
-    cy.contains('button', 'Sign In').click()
-
     //close/acknowledge the demo user banner
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
@@ -43,7 +37,6 @@ describe('m4n4ge-my-app: dashboard tests', () => {
 
     //check if certains fields are required and "... is required" error message is displayed
     cy.get('label').filter('.Mui-error').its('length').should('be.gt', 1)
-    //TODO: below is finding more elements than it appears in the UI, fix it later
     cy.get('body *').filter(':contains("is required")').each($el => {
       cy.wrap($el).should('be.visible')
     }).then($visibleElements => {
@@ -63,16 +56,6 @@ describe('m4n4ge-my-app: dashboard tests', () => {
   })
 
   it('should prevent demo user from deleting a new application while on dashboad page', () => {
-    cy.visit('/')
-
-    //click on the sign in button
-    cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
-
-    //fill the email and password fields and click on the sign in button
-    cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
-    cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
-    cy.contains('button', 'Sign In').click()
-
     //switch to expert user account as new user have no applications to delete
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
@@ -80,9 +63,7 @@ describe('m4n4ge-my-app: dashboard tests', () => {
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
     //check if expert user data is loaded
-    //1. check if the greating message uses the correct name
     cy.contains('h6', 'John').should('be.visible')
-    //2. check if the avatar displays the correct first letter of the name
     cy.get('div.MuiAvatar-root').should('contain', 'J')
 
     //find the fist application and click on it to get the delete button  and click it
@@ -92,7 +73,7 @@ describe('m4n4ge-my-app: dashboard tests', () => {
     cy.contains('h2#modal-modal-title', 'Delete Application').should('be.visible')
     cy.contains('p#modal-modal-description', 'Are you sure you want to delete').should('be.visible')
 
-    //click on the proceed button in the 
+    //click on the proceed button in the
     cy.contains('button', 'Proceed').click()
 
     //check if the user actions gets blocked and the error message is displayed
@@ -100,16 +81,6 @@ describe('m4n4ge-my-app: dashboard tests', () => {
   })
 
   it('should prevent demo user from deleting a new application while on edit application page', () => {
-    cy.visit('/')
-
-    //click on the sign in button
-    cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
-
-    //fill the email and password fields and click on the sign in button
-    cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
-    cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
-    cy.contains('button', 'Sign In').click()
-
     //switch to expert user account as new user have no applications to delete
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
@@ -117,12 +88,10 @@ describe('m4n4ge-my-app: dashboard tests', () => {
     cy.get('.css-14mo1hq > .MuiButton-root').click().wait(1000)
 
     //check if expert user data is loaded
-    //1. check if the greating message uses the correct name
     cy.contains('h6', 'John').should('be.visible')
-    //2. check if the avatar displays the correct first letter of the name
     cy.get('div.MuiAvatar-root').should('contain', 'J')
 
-    //find the fist application and click on it to get the delete button  and click it
+    //find the fist application and click on it to get the edit button  and click it
     cy.get('tbody>tr').first().click().contains('button', 'Edit').click()
 
     //check if the user is redirected to the edit application page
@@ -135,7 +104,7 @@ describe('m4n4ge-my-app: dashboard tests', () => {
     cy.contains('h2#modal-modal-title', 'Delete Application').should('be.visible')
     cy.contains('p#modal-modal-description', 'Are you sure you want to delete').should('be.visible')
 
-    //click on the proceed button in the 
+    //click on the proceed button in the
     cy.contains('button', 'Proceed').click()
 
     //check if the user actions gets blocked and the error message is displayed
@@ -143,16 +112,6 @@ describe('m4n4ge-my-app: dashboard tests', () => {
   })
 
   it('should prevent demo user from editing an existing application', () => {
-    cy.visit('/')
-
-    //click on the sign in button
-    cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
-
-    //fill the email and password fields and click on the sign in button
-    cy.get('input[name="email"]').type('new_user@m4n4gemy.app')
-    cy.get('input[name="password"]').type(Cypress.env('NEW_USER_PASSWORD'))
-    cy.contains('button', 'Sign In').click()
-
     //switch to expert user account as new user have no applications to edit
     cy.contains('label', 'John Doe (Expert User)').click().wait(1000)
 
@@ -176,5 +135,4 @@ describe('m4n4ge-my-app: dashboard tests', () => {
     cy.contains('div', 'Access Denied: Demonstration accounts do not have the privileges to update an application. Please create a personal account for full access.').should('be.visible')
     cy.location('pathname').should('include', '/edit')
   })
-
 })
