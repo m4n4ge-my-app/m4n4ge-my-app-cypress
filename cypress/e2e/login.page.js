060006551c686afabc2d261fa9646e843dc088e2
dinyar-qa <dinyar.qa@gmail.com>
2024-09-25 13:30:32 -0400

    refactor: use page object method to encapsulate login logic

--- cypress/e2e/login.page.js
+++ cypress/e2e/login.page.js
@@ -19,5 +19,17 @@ export const LoginPage = {
     },
     displayToastError(errorString) {
         cy.contains('div.MuiAlert-message', errorString).should('exist').and('be.visible')
+    },
+    login(username, password) {
+        cy.log(`Session for user: ${username}`)
+        cy.session('user session', () => {
+            cy.visit('/')
+            cy.get(':nth-child(1) > a > .MuiButtonBase-root').click()
+            cy.get('input[name="email"]').type(username)
+            cy.get('input[name="password"]').type(password, { log: false })
+            cy.contains('button', 'Sign In').click().wait(3000)
+            cy.window().then(win => win.localStorage.getItem('user'));
+        })
+        cy.visit('/dashboard')
     }
 }
